445b03026c607af7956b84e8e177de6225f0a63a
ryan shade <kyunoske@gmail.com>
2022-09-22 16:46:00 +0200

    add state, character filter, and axios get api

--- src/components/CharacterGallery.tsx
+++ src/components/CharacterGallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./CharacterGallery.css";
 import CharacterCard from "./CharacterCard";
 import {Character} from "../models/Character";
@@ -7,19 +7,36 @@ type CharacterGalleryProps = {
     characters: Character[];
 }
 
-
 function CharacterGallery(props: CharacterGalleryProps) {
+
+    const [data, setData] = useState("");
+    const filteredCharacters = props.characters.filter((character) => character.name.toLowerCase().includes(data));
+    const clearSearch = () => {
+        setData("");
+    }
+
     return (
-            <div className="cards">
-                    {props.characters.map((card, index) => {
-                        return <CharacterCard
-                            key={index}
-                            name={card.name}
-                            image={card.image}
-                            status={card.status}
-                        />
-                    })}
+        <div>
+            <div className="input-container">
+                <div className="input-group input-group-sm mb-3">
+                    <button onClick={clearSearch} className="input-group-text" id="inputGroup-sizing-sm">Clear</button>
+                    <input style={{width: "80%"}} type="text" placeholder="enter a name"
+                           onChange={(event ) => setData(event.target.value)} />
+                </div>
             </div>
+
+            <div className="cards">
+                {filteredCharacters.map((card, index) => {
+                    return <CharacterCard
+                        key={index}
+                        name={card.name}
+                        image={card.image}
+                        status={card.status}
+                    />
+                })}
+            </div>
+        </div>
+
     );
 }
 
